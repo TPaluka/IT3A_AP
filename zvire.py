777e4651a383955893f2fc4521b640ae18f167e5
TPaluka <shuzake2@email.cz>
2026-09-15 10:30:48 +0200

    uvod github

---
+++ zvire.py
@@ -0,0 +1,44 @@
+class Zvire:
+    def __init__(self, jmeno:str, vek:int, misto:str = "bouda"):
+        self.jmeno = jmeno
+        self.vek = vek
+        self.misto = misto
+        pass
+    
+    def zvuk(self):
+        return "???"
+    
+    def predstav_se(self):
+        return f"Jsem {self.jmeno} a je mi {self.vek}"
+    
+    def kde_jsi(self):
+        return f"Nacházím se {self.misto}"
+    
+    def jdi_na(self, nMisto:str):
+        self.misto = nMisto
+        return f"Šel jsem na {nMisto}"
+
+    
+zvire = Zvire("Šoral", 50)
+
+print(zvire.jmeno)
+print(zvire.vek)
+print(zvire.misto)
+
+print(zvire.zvuk())
+print(zvire.predstav_se())
+print(zvire.kde_jsi())
+print(zvire.jdi_na("Kadeřnictví"))
+print(zvire.kde_jsi())
+
+zvire2 = Zvire("Wolfram", 32, "PentHouse")
+
+print(zvire2.jmeno)
+print(zvire2.vek)
+print(zvire2.misto)
+
+print(zvire2.zvuk())
+print(zvire2.predstav_se())
+print(zvire2.kde_jsi())
+print(zvire2.jdi_na("Klokánek"))
+print(zvire2.kde_jsi())
